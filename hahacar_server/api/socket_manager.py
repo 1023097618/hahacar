3e231b1563c2bdb0d339ef05ca9aa4549087b76c
Evelyn Hwan <14089758+evelyn-hwan@user.noreply.gitee.com>
2025-03-11 23:18:24 +0800

    延长socket连接超时时间

--- hahacar_server/api/socket_manager.py
+++ hahacar_server/api/socket_manager.py
@@ -6,5 +6,7 @@ sio = socketio.AsyncServer(
     async_mode="asgi",
     cors_allowed_origins=[],
     logger=True,
-    engineio_logger=True
+    engineio_logger=True,
+    ping_interval=10,
+    ping_timeout=300,
 )
